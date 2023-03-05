e519025bde253f8f9d0a75b311f9157ac9e88875
efecanshin <123658710+efecanshin@users.noreply.github.com>
2023-03-05 18:18:40 +0300

    Add files via upload

---
+++ KarbonAyakİzi.js
@@ -0,0 +1,76 @@
+const quizData = [
+    {
+        question: "Yemek alışkanlıklarınızı tanımlar mısınız?",
+        a: " Her gün et tüketiyorum.",
+        b: "Öğünlerimde et tüketmiyorum.",
+        c: "Haftada birkaç kez et tüketiyorum.",
+    },
+    {
+        question: "Atıklarınızı geri dönüştürüyor musunuz?(Plastik, metaller, cam, kağıt ve karton)",
+        a: "Geri dönüştürüyorum.",
+        b: " Geri dönüştürmüyorum",
+    },
+    {
+        question: "Elektrik tüketim miktarınız aylık ortalama ne kadardır? (TL)",
+        a: "0-100",
+        b: "100-250 ",
+        c: "250-700",
+        d: "700+",
+    },
+    {
+        question: "Doğal gaz tüketim miktarınız aylık ortalama ne kadardır? (TL)",
+        a: "0-100",
+        b: "100-250",
+        c: "250-700",
+        d: "700+",
+    },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
